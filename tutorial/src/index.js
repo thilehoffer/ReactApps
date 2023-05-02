867dae4ae28311763b378cc77aed1f397f60bb72
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2023-05-02 14:12:37 -0400

    Nested Components

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const Greeting = () => {
-    return <h2>My First Component</h2>;
-}
+  return (
+    <div>
+      <Person />
+      <Message />
+    </div>
+  );
+};
+
+const Person = () => <h2>john doe</h2>;
+const Message = () => {
+  return <p>this is the message</p>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting />);
