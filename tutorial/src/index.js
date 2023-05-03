f7d0b62619c21e55c814d147ab32d5c2bc9808d7
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2023-05-03 15:55:18 -0400

    props - somewhat dynamic

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -3,15 +3,31 @@ import ReactDOM from 'react-dom/client';
 
 import './index.css';
 
-const title = "The Covenant of Water (Oprah's Book Club)";
-const author = 'Abraham Verghese';
-const img = './images/book-1.jpg';
+const firstBook = {
+  author: 'Abraham Verghese',
+  title: "The Covenant of Water (Oprah's Book Club)",
+  img: './images/book-1.jpg',
+};
+
+const secondBook = {
+  author: 'James Clear',
+  title: 'Atomic Habits',
+  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+};
 
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 };
