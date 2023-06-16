7a4634c43bf1f123fd8d410f6a82378d6fbc6484
Todd Hilehoffer <toddhilehoffer@yahoo.com>
2023-06-16 11:39:44 -0400

    book-list

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -2,44 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+const books = [
+  {
+    author: 'Abraham Verghese',
+    title: "The Covenant of Water (Oprah's Book Club)",
+    img: './images/book-1.jpg',
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+];
 
-const firstBook = {
-  author: 'Abraham Verghese',
-  title: "The Covenant of Water (Oprah's Book Club)",
-  img: './images/book-1.jpg',
-};
-
-const secondBook = {
-  author: 'James Clear',
-  title: 'Atomic Habits',
-  img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
-};
-
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book
-        author={firstBook.author}
-        title={firstBook.title}
-        img={firstBook.img}
-      />
-      <Book
-        author={secondBook.author}
-        title={secondBook.title}
-        img={secondBook.img}
-      />
+      {books.map((book) => {
+        const { img, title, author } = book;
+        return <Book img={img} title={title} author={author} />;
+        //return Book(book);
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
   console.log(props);
-  const { img, title, author } = props;
+  const { img, title, author, children } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author} </h4>
+      {children}
     </article>
   );
 };
